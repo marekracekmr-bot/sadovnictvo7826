310f9f51b85de0a9c636b4a47467c57747fd3046
marekracek <marekracek.mr@gmail.com>
2026-09-23 08:19:23 +0200

    doplnenie popinavých

--- src/data/plants/popinave_rastliny.js
+++ src/data/plants/popinave_rastliny.js
@@ -1,7 +1,19 @@
 const popinaveRastliny = [
   // Sem budeš pridávať rastliny
 
-  
+  {
+  id: "celastrus-scandens",
+  name: "Bršlenec popínavý",
+  latin: "Celastrus scandens",
+  family: "Bršlenovité (Celastraceae)",
+  category: "popinave",
+  images: [
+    "popinave_rastliny/Celastrus-scandens1.jpg",
+    "popinave_rastliny/Celastrus-scandens2.jpg",
+  ]
+
+},
+
   {
      id: "hedera-helix",
      name: "Brečtan popínavý",
@@ -15,6 +27,34 @@ const popinaveRastliny = [
   ]
      
    },
+
+   {
+  id: "parthenocissus-quinquefolia",
+  name: "Pavinič päťlaločný",
+  latin: "Parthenocissus quinquefolia",
+  family: "Viničovité (Vitaceae)",
+  category: "popinave",
+  images: [
+    "popinave_rastliny/Parthenocissus-quinquefolia1.jpg",
+    "popinave_rastliny/Parthenocissus-quinquefolia2.jpg",
+  ]
+
+},
+
+{
+  id: "wisteria-sinensis",
+  name: "Vistéria čínska",
+  latin: "Wisteria sinensis",
+  family: "Bôbovité (Fabaceae)",
+  category: "popinave",
+  images: [
+    "popinave_rastliny/Wisteria-sinensis1.jpg",
+    "popinave_rastliny/Wisteria-sinensis2.jpg",
+    "popinave_rastliny/Wisteria-sinensis3.jpg",
+    "popinave_rastliny/Wisteria-sinensis4.jpg",
+  ]
+},
+
 ];
 
 export default popinaveRastliny;
